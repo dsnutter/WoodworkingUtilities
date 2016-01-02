53966e024b91b95e211207f4dc12fe0d825f2efc
DSNutter <DSN@DSN-laptop.local>
2016-01-02 12:43:04 -0500

    Updated sample header/footer
    Updated same website files, such as paths, headers, footers, and index
    pages.

---
+++ html/header.inc.php
@@ -0,0 +1,10 @@
+<!-- Sample Woodworking Utilities Header -->
+<html>
+ <head>
+    <title>Woodworking Utilitied</title>
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/content/js.php');
+?>
+ </head>
+ <body>
+ <!-- page content starts here --> 
